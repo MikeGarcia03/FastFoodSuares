9904c5a4826c13715fcf4e14988913141b1dbb84
MikeGarcia03 <miguelangelgarciadesena@gmail.com>
2018-05-25 15:06:36 -0400

    Business presentation card finished.

---
+++ app/src/main/java/com/example/mg2013_0864/fastfoodsuares/FastFoodSuaresActivity.java
@@ -0,0 +1,13 @@
+package com.example.mg2013_0864.fastfoodsuares;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FastFoodSuaresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fast_food_suares);
+    }
+}
